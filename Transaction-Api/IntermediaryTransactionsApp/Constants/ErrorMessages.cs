011e26a7610e898c7909d6594e7eff9877ed4f65
tungnm <kakal0tdz04@gmail.com>
2025-02-12 08:25:05 +0700

    add class enum error message

---
+++ Transaction-Api/IntermediaryTransactionsApp/Constants/ErrorMessages.cs
@@ -0,0 +1,22 @@
+﻿namespace IntermediaryTransactionsApp.Constants
+{
+	public enum ErrorMessages
+	{
+		InvalidCredentials, 
+		ObjectNotFound,
+
+	}
+
+	public static class ErrorMessageExtensions
+	{
+		public static string GetMessage(this ErrorMessages error)
+		{
+			return error switch
+			{
+				ErrorMessages.InvalidCredentials => "Invalid username or password.",
+				ErrorMessages.ObjectNotFound => "Object not found.",
+				_ => throw new NotImplementedException()
+			};
+		}
+	}
+}
--- Transaction-Api/IntermediaryTransactionsApp/Dtos/ApiDTO/ApiResponse.cs
+++ Transaction-Api/IntermediaryTransactionsApp/Dtos/ApiDTO/ApiResponse.cs
@@ -5,7 +5,7 @@
 	{
 		public int Code {  get; set; }
 		public string Message { get; set; }
-		public T Data { get; set; }
+		public T? Data { get; set; }
 
 		public ApiResponse(int code, string message, T data)
 		{
